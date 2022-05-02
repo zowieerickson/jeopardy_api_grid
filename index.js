91c81c0bc4783e62f0dec87f9ce827948b483695
Zowie Erickson <zowie.erickson@gmail.com>
2022-05-01 21:16:57 -0400

    created jeopardy layout

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ async function getCategories() {
 function getCategoryHtml(category) {
     return `
         <div class="my-category-title">${category.title}</div>
-        <div class="my-category-clue">$100</div>
-        <div class="my-category-clue">$200</div>
-        <div class="my-category-clue">$300</div>
-        <div class="my-category-clue">$400</div>
+        <div class="my-category-clue" style="grid-row-start: 2">$100</div>
+        <div class="my-category-clue" style="grid-row-start: 3">$200</div>
+        <div class="my-category-clue" style="grid-row-start: 4">$300</div>
+        <div class="my-category-clue" style="grid-row-start: 5">$400</div>
     `;
 }
 
